7b6ea91a5c2d8598ee0c0322de6e67ea372169a9
Aleksandar Diklic <rastasheep3@gmail.com>
2013-12-26 21:05:23 +0100

    Added validation for task title

--- models/task.go
+++ models/task.go
@@ -1,10 +1,15 @@
 package main
 
+import "fmt"
+
 type Task struct {
 	Title string
 	Done  bool
 }
 
-func NewTask(title string) *Task {
-	return &Task{title, false}
+func NewTask(title string) (*Task, error) {
+	if len(title) == 0 {
+		return nil, fmt.Errorf("empty title")
+	}
+	return &Task{title, false}, nil
 }
--- models/task_test.go
+++ models/task_test.go
@@ -4,11 +4,20 @@ import "testing"
 
 func TestNewTask(t *testing.T) {
 	title := "Make To-do app"
-	task := NewTask(title)
+	task, err := NewTask(title)
 	if task.Title != title {
 		t.Errorf("expected title %q, got %q", title, task.Title)
 	}
 	if task.Done {
 		t.Errorf("new task is done")
+	}
+	if err != nil {
+		t.Errorf("task should be crated")
+	}
+}
+func TestNewTaskEmptyTitle(t *testing.T) {
+	_, err := NewTask("")
+	if err == nil {
+		t.Errorf("task with empty title created")
 	}
 }
